5a6360ab860762ad27d14dbdf31f05afbe408311
Maël Drapier <mael.drapier@epitech.eu>
2020-01-28 01:03:42 -0500

    adding fields to the setup.py file

--- setup.py
+++ setup.py
@@ -7,7 +7,17 @@ setup(
     url="https://github.com/MaelDrapier/MusicalNotes",
     author="Maël Drapier",
     author_email="mael.drapier@gmail.com",
+    keywords="music musical note notes play player sound frequency",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Multimedia :: Sound/Audio :: Players"
+    ],
     packages=find_packages(),
+    install_requires=["pyaudio", "numpy"],
     entry_points={
         "console_scripts": [
             "musicalnotes=musicalnotes.script:main",
